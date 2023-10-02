abb7715bee9714b0e123ba098538105224d02099
MichaelKuswanto <90759961+MichaelKuswanto@users.noreply.github.com>
2023-10-02 14:26:43 +0700

    Add files via upload

--- Tugas02.py
+++ Tugas02.py
@@ -24,12 +24,18 @@ class TextSeq(StateMachine):
         if state == 0 and inp == 'bagaimana cuaca di bandung?':
             return (1, "Cuaca di bandung sekarang sedang berawan")
         elif state == 0 and inp == 'rekomendasi makanan di bandung':
-            return (1, "sate, seblak, cireng, dsb")
+            return (2, "sate, seblak, cireng, dsb")
         elif state == 1 and inp == 'apakah akan turun hujan?':
-            return (2, "Perkiraan cuaca 1 jam kedepan tidak turun hujan")
-        elif state == 1 and inp == 'warung seblak terdekat':
-            return (2, "warung seblak terdekat ada di jalan babakan jeruk")
-        elif state == 2 and inp == 'saya mau bertanya yang lain':
+            return (1.1, "Perkiraan cuaca 1 jam kedepan tidak turun hujan")
+        elif state == 1 and inp == 'apakah cuaca akan cerah?':
+            return (1.2, "Perkiraan cuaca 1 jam kedepan cerah")
+        elif state == 2 and inp == 'warung seblak terdekat':
+            return (2.1, "warung seblak terdekat ada di jalan babakan jeruk")
+        elif state == 2 and inp == 'penjual sate terdekat':
+            return (2.2, "penjual sate terdekat ada di surya sumantri")
+        elif state == 2 and inp == 'penjual cireng terdekat':
+            return (2.3, "penjual cireng terdekat ada di jalan cibogo atas")
+        elif (state == 1.1 or state == 1.2 or state == 2.1 or state == 2.2 or state == 2.3) and inp == 'saya mau bertanya yang lain':
             return (0, "Oke silahkan bertanya lagi")
         else:
             return (3, "Terimakasih")
